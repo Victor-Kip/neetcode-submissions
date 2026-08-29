05cb89173a5edb08cda34e3b8d0b11bfa860ffe0
Victor-Kip <131675484+Victor-Kip@users.noreply.github.com>
2026-08-29 19:29:59 +0300

    Add: binary-tree-right-side-view - submission-1

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-1.ts
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+    rightSideView(root: TreeNode | null): number[] {
+        let result:number[] = []
+        let myQ:TreeNode[] = []
+        myQ.push(root);
+        while(myQ.length != 0){
+            let qLength = myQ.length;
+            let rightSide = null;
+            for(let i = 0; i < qLength;i++){  
+                let node = myQ.shift()
+                if(node){
+                    rightSide = node;
+                    myQ.push(node.left)
+                    myQ.push(node.right)
+                }}
+                if(rightSide) result.push(rightSide.val);
+            }
+            
+        return result
+    }
+}
